8584f4c9a8de96e862f30b9f6e7d4401312acee9
Nehemias Ulloa <nulloa1@iastate.edu>
2017-10-02 22:27:51 -0500

    Upload basic common multivariate script

--- R/asg_multi.R
+++ R/asg_multi.R
@@ -17,6 +17,8 @@
 #' @return A MCMC object
 #'
 #' @examples
+#' mean.vector = c(beta1, beta2, leta, mu, lsigma1, lsigma2)
+#' 
 #' priors <- list()
 #' priors$mu0  # A 6x1 vector
 #' priors$phi0 # A 6x6 inverse var-cov matrix
@@ -49,20 +51,14 @@ asg_multi <- function(y, x, count, group, priors, niter, nchains=3, burnin=niter
   for (i in 1:n) {
     y[i] ~ dbinom(theta[i], num[i])
     logit(theta[i]) <- ltheta[i]
-    u[i] = ifelse(x[i] < mu[group[i]], 1, 0)
-    ltheta[i] = u[i]*(beta1[group[i]] + (eta[group[i]] - beta1[group[i]])*exp(-(x[i] - mu[group[i]])^2 / (2*sigma1[group[i]]^2))) + (1-u[i])*(beta2[group[i]] + (eta[group[i]]-beta2[group[i]])*exp(-(x[i] - mu[group[i]])^2 / (2*sigma2[group[i]]^2)))
+    u[i] = ifelse(x[i] < ctheta[3], 1, 0)
+    ltheta[i] = u[i]*(ctheta[1] + (exp(ctheta[3]) - ctheta[1])*exp(-(x[i] - ctheta[4])^2 / (2*exp(ctheta[5]^2)))) + (1-u[i])*(ctheta[2] + (exp(ctheta[3])-ctheta[2])*exp(-(x[i] - ctheta[4])^2 / (2*exp(ctheta[6]^2))))
   }
   
-  for (g in 1:nG) {
-    log(eta[g]) <- leta[g]
-    log(sigma1[g]) <- lsigma1[g]
-    log(sigma2[g]) <- lsigma2[g]
-    ctheta[g,] <- c(beta1[g], beta1[g], leta[g], mu[g], lsigma1[g], lsigma2[g])
-    ctheta[g, 1:6] ~ dmnorm(mu0, phi0)
-  }
+  ctheta[1:6] ~ dmnorm(mu0, phi0) # c(beta1[g], beta2[g], leta[g], mu[g], lsigma1[g], lsigma2[g])
   
   }"
   m = jags.model(textConnection(Multi), data=dat, n.chains=nchains, n.adapt=burnin)
-  res = coda.samples(m, c("eta","mu","ltheta","beta1","beta2","theta","sigma1","sigma2"), niter, thin=thin)
+  res = coda.samples(m, c("ctheta","theta","ltheta"), niter, thin=thin)
   return(res)
 }
