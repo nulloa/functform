b5fb50e4bf02e10c458d97583087dc76566e2ce5
Nehemias Ulloa <nulloa1@iastate.edu>
2017-10-19 15:48:23 -0500

    Subset priors from dat

--- R/drake_mcmc.R
+++ R/drake_mcmc.R
@@ -23,43 +23,44 @@
 drake_mcmc <- function(d, iter = 1000, chains = 3, clusters=3, burnin = iter/2, thin=10, whichmodel=NULL){
     if(is.null(whichmodel)) stop("No model was chosen.")
     require(functform)
-    dat <- d$dat
-    priors <- d$priors
+    dat <- d[c(1,2,3,4)]
+    priors <- d[-c(1,2,3,4)]
+    
     if(whichmodel=="asg_common"){
-      s <- asg_common(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- asg_common(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="asg_common_ln"){
-      s <- asg_commonln(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- asg_commonln(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="asg_indep"){
-      s <- asg_indep(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- asg_indep(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="asg_indep_ln"){
-      s <- asg_indepln(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- asg_indepln(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="asg_hier"){
-      s <- asg_hier(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- asg_hier(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="asg_hier_ln"){
-      s <- asg_hierln(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- asg_hierln(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="ln_common"){
-      s <- lognormal_common(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- lognormal_common(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="ln_indep"){
-      s <- lognormal_indep(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- lognormal_indep(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="ln_hier"){
-      s <- lognormal_hier(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- lognormal_hier(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="n_common"){
-      s <- normal_common(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- normal_common(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="n_indep"){
-      s <- normal_indep(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- normal_indep(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     if(whichmodel=="n_hier"){
-      s <- normal_hier(dat$y, dat$week, dat$num, dat$region, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
+      s <- normal_hier(dat$y, dat$week, dat$num, dat$group, priors, nchains=chains, ncluster=clusters, niter=iter, burnin=burnin, thin=thin)
     }
     
     return(s)
